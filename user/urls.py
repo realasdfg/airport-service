601bbfe02e1df746513d47e02ffb877e1625b65c
Roman <realasdfga@gmail.com>
2026-07-09 00:54:04 +0200

    Implement user me/ endpoint

--- user/urls.py
+++ user/urls.py
@@ -4,12 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 
 app_name = "user"
