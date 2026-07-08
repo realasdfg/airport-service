1d35860bdae58a6c36964176d0850ded03b5cb54
Roman <realasdfga@gmail.com>
2026-07-09 01:04:03 +0200

    Add a JWT verify endpoint

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from user.views import CreateUserView, ManageUserView
@@ -10,6 +11,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 
