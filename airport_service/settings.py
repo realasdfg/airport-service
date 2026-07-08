8efad9a77ed0cbab1569bd0d0e4ab40a48dd565a
Roman <realasdfga@gmail.com>
2026-07-09 00:41:56 +0200

    Implement user register endpoint and JWT support

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "user",
     "airport",
 ]
@@ -118,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
